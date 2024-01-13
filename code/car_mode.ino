4ea248c47c82ae5abbd30e3898391402a97fe2e8
Q60module <156203050+Q60module@users.noreply.github.com>
2024-01-13 17:22:11 -0500

    Create car_mode.ino

---
+++ code/car_mode.ino
@@ -0,0 +1,37 @@
+©2024 by David Horton. This work is openly licensed via <a href="https://github.com/Q60module/PersonalMode/blob/main/LICENSE.txt">CC BY 4.0</a> 
+
+#include <ArduinoLowPower.h>
+
+#define SWITCH1 A6  //switch pin, used to talk to the Relay
+
+void printSetup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(F("Starting"));
+  Serial.println();
+  Serial.flush();
+}
+
+void setup() {
+  pinMode(1, OUTPUT);
+  delay(20000);
+  for (int i=0; i<3; i++) {
+    digitalWrite(1, HIGH);
+    delay(200);
+    digitalWrite(1, LOW);
+    delay(700);
+  }
+  pinMode(SWITCH1, INPUT);
+
+  LowPower.attachInterruptWakeup(SWITCH1, wakeUp, CHANGE);  //setup interrupt for wake up
+}
+
+void loop() {
+  LowPower.deepSleep();
+}
+
+void wakeUp()
+{
+  //handler for the interrupt
+  LowPower.deepSleep();
+}
